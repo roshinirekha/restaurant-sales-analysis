4cbfdd84cbcb11392153135061fc630b26500c29
roshinirekha <117009825+roshinirekha@users.noreply.github.com>
2026-05-20 12:51:19 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sales (
+Date DATE,
+Time TIME,
+Order_ID VARCHAR(20),
+Table_Number INT,
+Item_Name VARCHAR(100),
+Category VARCHAR(50),
+Quantity INT,
+Unit_Price FLOAT,
+Total_Sale FLOAT,
+Payment_Method VARCHAR(50),
+Server_Name VARCHAR(50),
+Customer_Type VARCHAR(50),
+Dine_Type VARCHAR(50)
+);
